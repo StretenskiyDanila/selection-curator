dda06071983452429e748d7e1834e105fe573d94
TheMadman <97761847+StretenskiyDanila@users.noreply.github.com>
2023-07-19 11:37:08 +0500

    Update SecondB.java

--- 2/SecondB.java
+++ 2/SecondB.java
@@ -1,6 +1,6 @@
 public class SecondB {
     public static void main(String[] args) {
-        for (int i = 13211; i < 1012010201; i++) {
+        for (int i = 13211; i < 98342; i++) {
             double sqrt1 = Math.sqrt(i);
             if (sqrt1 == Math.ceil(sqrt1)) {
                 double sqrt2 = Math.sqrt(sqrt1);
