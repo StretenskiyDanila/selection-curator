94727585e8b243ea6b89c76019962ab5e4a75f27
TheMadman <97761847+StretenskiyDanila@users.noreply.github.com>
2023-07-19 12:11:49 +0500

    Update ThirdA.java

--- 3/ThirdA.java
+++ 3/ThirdA.java
@@ -1,1 +1,11 @@
-
+public class ThirdA {
+    public static void main(String[] args) {
+        String s = "2?953*6";
+        for (int i = 2024; i < 299539997; i += 2024) {
+            String tempI = String.valueOf(i);
+            if (i % 10 == 6 && tempI.charAt(0) == s.charAt(0) && tempI.substring(2, 5).equals(s.substring(2,5)) && i % 552 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
